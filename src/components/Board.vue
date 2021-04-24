a003af05c0166a4bcf819897d9a1211259966e2b
Jakub Chrupek <jakub.chrupek@hotmail.com>
2021-04-24 20:14:33 +0200

    Added a div for content

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,22 +1,22 @@
 <template>
   <main
-    class="col-span-full grid grid-cols-12 row-span-1 h-3/5 w-full rounded bg-white mt-36 py-12"
+    class="col-span-full grid grid-cols-12 row-span-1 h-auto w-full rounded bg-white mt-36 py-12"
   >
     <h1 class="col-span-full text-center text-4xl">Go-Todo</h1>
-    <slot></slot>
+    <div class="col-span-10 col-start-2 row-start-2 h-96">
+      <slot></slot>
+    </div>
 
-    <Button class="col-span-4 row-start-3 col-start-5 ">
-        Add new Todo
-    </Button>
+    <Button class="col-span-4 row-start-3 col-start-5"> Add new Todo </Button>
   </main>
 </template>
 
 <script lang="ts">
 import Button from "./basic/Button.vue";
 export default {
-    components: {
-        Button
-    }
+  components: {
+    Button,
+  },
 };
 </script>
 
