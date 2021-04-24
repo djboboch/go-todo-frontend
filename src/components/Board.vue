f18f9c53fc1beef07e9dd8e5559c5625b44c3c42
Jakub Chrupek <jakub.chrupek@hotmail.com>
2021-04-24 19:54:25 +0200

    Created the board component

---
+++ src/components/Board.vue
@@ -0,0 +1,23 @@
+<template>
+  <main
+    class="col-span-full grid grid-cols-12 row-span-1 h-3/5 w-full rounded bg-white mt-36 py-12"
+  >
+    <h1 class="col-span-full text-center text-4xl">Go-Todo</h1>
+    <slot></slot>
+
+    <Button>
+        Add new Todo
+    </Button>
+  </main>
+</template>
+
+<script lang="ts">
+import Button from "./basic/Button.vue";
+export default {
+    components: {
+        Button
+    }
+};
+</script>
+
+<style></style>
