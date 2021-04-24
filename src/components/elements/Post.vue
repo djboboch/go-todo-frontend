5fe2e41966f16f4053f619ec8fc3889ccf7ecb34
Jakub Chrupek <jakub.chrupek@hotmail.com>
2021-04-24 14:49:34 +0200

    added psort component

---
+++ src/components/elements/Post.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="w-full border rounded mb-4 pt-2 pb-2 px-4">
+    <div>
+      {{ postData.header}}
+    </div>
+    <span class="block h-1 bg-red-800"></span>
+    <div>
+      {{ postData.content }}
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { PropType } from "@vue/runtime-core";
+
+export interface PostData {
+  id: string;
+  header: string;
+  content: string;
+}
+
+export default {
+  props: {
+    postData: {
+      type: Object as PropType<PostData>,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style></style>
