196ba275b7df6c83a6b3a704d263042d4f4bbc15
Jakub Chrupek <jakub.chrupek@hotmail.com>
2021-04-24 14:49:46 +0200

    Added Post list

---
+++ src/components/PostList.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="col-span-6 col-start-4 flex flex-col">
+    <Post v-for="post in postList" :key="post.id" 
+    :postData="post"/>
+  </div>
+</template>
+
+<script lang="ts">
+import Post, { PostData } from "./elements/Post.vue";
+
+let postList: PostData[] = [
+  {
+    id: "1",
+    header: "Test header",
+    content: "Example content of the body",
+  },
+  {
+    id: "2",
+    header: "Test header 2",
+    content: "Example content of the body",
+  },
+  {
+    id: "3",
+    header: "Test header 3",
+    content: "Example content of the body",
+  },
+];
+
+export default {
+  data() {
+    return {
+      postList,
+    };
+  },
+  components:{
+      Post
+  }
+};
+</script>
+
+<style></style>
