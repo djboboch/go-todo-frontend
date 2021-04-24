95bdbeca2156bf58d446eacdd07413eb5a4d47f2
Jakub Chrupek <jakub.chrupek@hotmail.com>
2021-04-24 19:56:15 +0200

    moved the button positon to board

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -5,7 +5,7 @@
     <h1 class="col-span-full text-center text-4xl">Go-Todo</h1>
     <slot></slot>
 
-    <Button>
+    <Button class="col-span-4 row-start-3 col-start-5 ">
         Add new Todo
     </Button>
   </main>
--- src/components/basic/Button.vue
+++ src/components/basic/Button.vue
@@ -1,6 +1,6 @@
 <template>
   <button
-      class="col-span-4 row-start-3 col-start-5 rounded py-3 bg-indigo-600 hover:bg-indigo-700 shadow-lg text-white uppercase font-bold"
+      class="rounded py-3 bg-indigo-600 hover:bg-indigo-700 shadow-lg text-white uppercase font-bold"
       @click="$emit('click')"
     >
       <slot>
