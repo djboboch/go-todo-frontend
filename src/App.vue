78a4ac9a6ac431cd457bef883355400708c043e5
Jakub Chrupek <jakub.chrupek@hotmail.com>
2021-04-25 11:16:02 +0200

    Using Post List

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container mx-auto grid grid-cols-12">
     <Board>
-      dsd
+      <PostList />
     </Board>
   </div>
 </template>
@@ -9,11 +9,13 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import Board from "./components/Board.vue";
+import PostList from "./components/PostList.vue"
 
 export default defineComponent({
   name: "App",
   components: {
     Board,
+    PostList
   },
 });
 </script>
